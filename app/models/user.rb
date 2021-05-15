a657271b74c569f8a0611e53fab684ec6f74c4eb
Colin Densem <colin@summit360.co.uk>
2021-05-15 11:15:07 +0100

    lint: standardrb pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable,
     :confirmable, :lockable, :timeoutable, :trackable
-
-
 end
